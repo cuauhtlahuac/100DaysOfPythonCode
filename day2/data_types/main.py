4e66b14ea14613c55c3b0667f28222059a3f9991
Rodrigo macos sierra <cuitlagua@gmail.com>
2021-11-02 23:01:52 -0600

    100py: Primitive data types

---
+++ day2/data_types/main.py
@@ -0,0 +1,18 @@
+# Primitive Data Types
+
+#Strings
+
+print("Hello"[-1]) # Direct access to the index of the string, it's know as substring
+
+# Integer
+
+print(123 + 345)
+
+# Float
+
+print(3141.59)
+
+# Boolean
+
+True
+False

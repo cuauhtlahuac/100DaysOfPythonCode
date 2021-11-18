cc300fadc8d9620c8975c93299864adcec60d48f
Rodrigo macos sierra <cuitlagua@gmail.com>
2021-11-17 23:22:59 -0600

    100py: day-3-4-exercise improved

--- day3/day-3-4-exercise/main.py
+++ day3/day-3-4-exercise/main.py
@@ -3,9 +3,9 @@ from typing import final
 
 
 print("Welcome to Python Pizza Deliveries!")
-size = input("What size pizza do you want? S, M, or L: ").capitalize()
-add_pepperoni = input("Do you want pepperoni? Y or N: ").capitalize()
-add_extra_cheese = input("Do you want extra cheese? Y or N: ").capitalize()
+size = input("What size pizza do you want? S, M, or L: ")[0].capitalize()
+add_pepperoni = input("Do you want pepperoni? Y or N: ")[0].capitalize()
+add_extra_cheese = input("Do you want extra cheese? Y or N: ")[0].capitalize()
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
@@ -23,9 +23,9 @@ extra_cheese = 1
 
 if(size == "S"):
   final_bill += small_pizza
-if size == "M":
+elif size == "M":
   final_bill += medium_pizza
-if size == "L":
+else:
   final_bill += large_pizza
 
 if(add_pepperoni == "Y"):

c492fcab5a988756ed3d043b9a8ccac18b51c486
Rodrigo macos sierra <cuitlagua@gmail.com>
2021-11-24 23:23:03 -0600

    100py: fix folder and creating my first py module!

--- day4/ day-4-start/main.py
+++
@@ -1,4 +0,0 @@
-import random
-
-new_int = random.randint(1, 10)
-print(new_int)
---
+++ day4/day-4-start/main.py
@@ -0,0 +1,7 @@
+import random
+import my_module
+
+new_int = random.randint(1, 10)
+print(new_int)
+
+print(my_module.pi)

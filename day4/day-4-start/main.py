617782bb5becef57610bdcaacdc65062a355b3e6
Rodrigo macos sierra <cuitlagua@gmail.com>
2021-11-25 23:19:15 -0600

    100py: random float

--- day4/day-4-start/main.py
+++ day4/day-4-start/main.py
@@ -4,4 +4,12 @@ import my_module
 new_int = random.randint(1, 10)
 print(new_int)
 
-print(my_module.pi)
+random_float = random.random() # It's generate floating numbers between 0.0 and a number before 1.0
+print(random_float)
+
+random_float * 5 # here we have a trick to get a random number between 0 and 5 
+                 # because the multiplication with floating numbers never gonna reach the 5
+                 # for example 0.99999 * 5 is equal to 4.999995
+
+love_score = random.randint(1, 100)
+print(f"Your love score is {love_score}") # easy way to do a previous game

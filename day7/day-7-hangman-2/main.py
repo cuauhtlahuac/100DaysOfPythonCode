024a82ab002ba4e517bc4728eea265bd7cf75ef8
Rodrigo macos sierra <cuitlagua@gmail.com>
2021-12-19 14:46:19 -0600

    fix: change the for in array to range and len

--- day7/day-7-hangman-2/main.py
+++ day7/day-7-hangman-2/main.py
@@ -21,13 +21,14 @@ guess = input("Guess a letter: ")[-1].lower()
 #TODO-2: - Loop through each position in the chosen_word;
 #If the letter at that position matches 'guess' then reveal that letter in the display at that position.
 #e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
-index = 0
-for letter in chosen_word:
+
+for position in range(len(chosen_word)):
+    letter = chosen_word[position]
     if letter == guess:
-        display[index] = guess
+        display[position] = guess
     else:
         print("")
-    index += 1
+
 
 #TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
 #Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.

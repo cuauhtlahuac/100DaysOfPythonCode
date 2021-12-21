86421d46818232359f3bfe890d7f5a45c2dbaee9
Rodrigo macos sierra <cuitlagua@gmail.com>
2021-12-20 23:18:00 -0600

    100py: hangman make the loose logic

--- day7/day-7-hangman-3/main.py
+++ day7/day-7-hangman-3/main.py
@@ -1,35 +1,42 @@
-#Step 2
+# Step 2
 
 import random
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 
-#Testing code
+# Testing code
 print(f'Pssst, the solution is {chosen_word}.')
 
 display = []
-#For each letter in the chosen_word, add a "_" to 'display'.
+
 for letter in chosen_word:
     display.append("_")
 
-# TODO-1: - Use a while loop to let the user guess again. 
-# The loop should only stop once the user has guessed all
-
 user_guess = True
+# You can give more live by adding a integer
+lives = len(display)
 
 while user_guess:
-  guess = input("Guess a letter: ")[-1].lower()
-  for position in range(len(chosen_word)):
-      letter = chosen_word[position]
-      if letter == guess:
-          display[position] = guess
+    guess = input("Guess a letter: ")[-1].lower()
+    numberOfGuesses = 0
+    for position in range(len(chosen_word)):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = guess
+            numberOfGuesses += 1
 
-  print(display)
-  # the letters in the chosen_word and 'display' has no more blanks ("_").
-  try:
-    display.index("_")
-  except:
-  # Then you can tell the user they've won.
-    user_guess = False
-    print("You win!")
-  
+    lives -= numberOfGuesses if (numberOfGuesses > 0) else 1
+
+    print(display, lives, numberOfGuesses)
+
+    if lives == 0:
+        print("you loose")
+        user_guess = False
+
+    # the letters in the chosen_word and 'display' has no more blanks ("_").
+    try:
+        display.index("_")
+    except:
+        # Then you can tell the user they've won.
+        user_guess = False
+        print("You win!")

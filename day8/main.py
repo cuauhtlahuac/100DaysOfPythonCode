c6480c9250333121b871f45a214cc007d89bfc90
Rodrigo macos sierra <cuitlagua@gmail.com>
2021-12-26 12:35:15 -0600

    100py: function with arguments start

---
+++ day8/main.py
@@ -0,0 +1,16 @@
+# Review: 
+# Create a function called greet(). 
+def greet():
+# Write 3 print statements inside the function.
+  print("Greetings")
+  print("Fellow")
+  print("Humans")
+# Call the greet() function and run your code.
+greet()
+
+def greet_with_name(name):
+  print(f"Hello {name}")
+  print(f"How do you do {name}")
+  print(f"Isn't the weather nice today?")
+
+greet_with_name("Jack")

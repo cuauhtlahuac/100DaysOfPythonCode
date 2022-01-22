67128091237ed643c650bc68724f2433eb5e6005
Rodrigo macos sierra <cuitlagua@gmail.com>
2022-01-21 23:12:40 -0600

    100py: calculator, creating function operations

--- day10/day-10-3-1-calculator-start/main.py
+++ day10/day-10-3-1-calculator-start/main.py
@@ -1,3 +1,19 @@
 from art import logo
 
 print(logo)
+
+# add
+def add(n1, n2):
+    return n1 + n2
+
+# substract
+def substract(n1, n2):
+    return n1 - n2
+
+# multiply
+def multiply(n1, n2):
+    return n1 * n2
+
+# divide
+def divide(n1, n2):
+    return n1 / n2

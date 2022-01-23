90cb197f08661a168ba2fc62ad7350dcadce3376
Rodrigo macos sierra <cuitlagua@gmail.com>
2022-01-23 13:27:49 -0600

    100py: calculator, create an operations dictionary

--- day10/day-10-3-1-calculator-start/main.py
+++ day10/day-10-3-1-calculator-start/main.py
@@ -17,3 +17,12 @@ def multiply(n1, n2):
 # divide
 def divide(n1, n2):
     return n1 / n2
+
+# Create a dictionary, where we store the operation sign as value  and the operator as key
+
+operations = {
+    add: '+',
+    substract: '-',
+    multiply: '*',
+    divide: '/'
+}

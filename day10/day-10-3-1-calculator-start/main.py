5fb4e49751f79ef0b43453a524d96c3a83362d14
Rodrigo macos sierra <cuitlagua@gmail.com>
2022-01-27 10:00:29 -0600

    100py: refactor: calculator, continues operator

--- day10/day-10-3-1-calculator-start/main.py
+++ day10/day-10-3-1-calculator-start/main.py
@@ -32,30 +32,20 @@ num1 = int(input("What's the first number?: "))
 for symbol in operations:
     print(symbol)
 
-operation_symbol = input('Pick an operation from the line above: ')
-function = operations[operation_symbol] # here we assign the function
+should_continue = True
 
-num2 = int(input("What's the second number?: "))
-
-answer = function(num1, num2) # we call the function
-
-print(f"{num1} {operation_symbol} {num2} = {answer}")
-
-continue_with_answer = False
-want_exit = input(f"Type 'y' to continue calculation with {answer}, or type 'n' to exit.: ")[0].lower()
-if(want_exit == 'y'):
-    continue_with_answer = True
-while continue_with_answer:
-    operation_symbol = input('Pick an operation: ')
+while should_continue:
+    operation_symbol = input('Pick an operation (+ ,- ,* ,/ ): ')
     function = operations[operation_symbol] # here we assign the function
-    num3 = int(input("What's the second number?: "))
-    second_answer = function(answer, num3) # as we use the value returned we can reuse here again
 
-    print(f"{answer} {operation_symbol} {num3} = {second_answer}")
+    num2 = int(input("What's the next number?: "))
 
-    want_exit = input(f"Type 'y' to continue calculation with {second_answer}, or type 'n' to exit.: ")[0].lower()
-    if(want_exit == 'y'):
-        continue_with_answer = True
-        answer = second_answer
-    else:
-        continue_with_answer = False
+    answer = function(num1, num2) # we call the function
+
+    print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+    want_exit = input(f"Type 'y' to continue calculation with {answer}, or type 'n' to exit.: ")[0].lower()
+    num1 = answer
+    if(want_exit != 'y'):
+        should_continue = False
+ 

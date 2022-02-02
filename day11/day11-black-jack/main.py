fcc7f020ff97cf95c373f207c5645d5d394fb738
Rodrigo macos sierra <cuitlagua@gmail.com>
2022-02-01 22:29:22 -0600

    100py: black jack start

---
+++ day11/day11-black-jack/main.py
@@ -0,0 +1,9 @@
+# if you get over 21 you lose
+
+# J Q and K have 10 point each
+
+# As value is 1 or 11, for example 10 + as could be 21 or 11
+
+# regular cards goes from 2 to 9
+
+# if the number of both is the same, there is a draw

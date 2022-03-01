52ecf80e24c5065ce1d8444b112cfe9f5fd52dfd
Rodrigo macos sierra <cuitlagua@gmail.com>
2022-02-28 23:36:06 -0600

    100py: game working but not finished

--- day14/day14-game/main.py
+++ day14/day14-game/main.py
@@ -1,19 +1,50 @@
 from art import logo, vs
-from game_data import data
+from game_data import data as original_data
 from random import choice
+from game_utilities import win_calculator
+
+data = original_data
+
+final_score = 0
 
 print(logo)
 
-a_data = choice(data)
-b_data = choice(data)
+game_active = True
 
-first_choice = f"Compare A: {a_data['name']}, a {a_data['description']}, from {a_data['country']}."
-second_choice = f"\nCompare B: {b_data['name']}, a {b_data['description']}, from {b_data['country']}."
+both_data = {}
 
-print(first_choice)
+round_number = 0
+while game_active:
+    choices = ['A', 'B']
+    if round_number == 0:
+      a_data = choice(data)
+    both_data[choices[0]] = a_data 
+    b_data = choice(data)
+    both_data[choices[1]] = b_data
 
-print(vs)
+    first_choice = f"Compare A: {a_data['name']}, a {a_data['description']}, from {a_data['country']}."
+    second_choice = f"\nCompare B: {b_data['name']}, a {b_data['description']}, from {b_data['country']}."
 
-print(second_choice)
+    print(first_choice)
 
-# I have to find the way to burn the lower choice in order to don't show it again
+    print(vs)
+
+    print(second_choice)
+
+    user_guess_char = input(f"Who has more followers? Type '{choices[0]}' or '{choices[1]}': ")[0].upper()
+    choices.pop(0) if user_guess_char == choices[0] else choices.pop(1)
+    user_guess = both_data[user_guess_char]
+    not_selected = both_data[choices[0]]
+
+    print(f"\nYour guess = {user_guess['name']} : {user_guess['follower_count']}")
+    print(f"vs = {not_selected['name']} : {not_selected['follower_count']}")
+
+    if(win_calculator(user_guess['follower_count'], not_selected['follower_count'])):
+      print('\nCorrect! \n')
+      final_score += 1
+      a_data = user_guess
+      round_number += 1
+      # add a data cleaner of the not selected answer
+    else:
+      print(f"\nSorry, that's wrong. Final score: {final_score}")
+      game_active = False

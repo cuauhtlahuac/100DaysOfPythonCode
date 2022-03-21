a42e2d52908448b567bf22e7db3463771c4bb7be
Rodrigo macos sierra <cuitlagua@gmail.com>
2022-03-20 22:05:21 -0600

    100py: print the attribute canvas height from screen object

--- day16/main.py
+++ day16/main.py
@@ -1,7 +1,11 @@
 import another_module
-from turtle import Turtle
+from turtle import Turtle, Screen
 
 print(another_module.another_variable)
 
 # let's create a new instance of turtle
 timmy = Turtle()
+
+screen = Screen()
+
+print(screen.canvheight)

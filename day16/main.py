e32ee211438b2d6017f7e5a9b1ea748b4772ae48
Rodrigo macos sierra <cuitlagua@gmail.com>
2022-03-22 22:34:28 -0600

    100py: stop spinning when click

--- day16/main.py
+++ day16/main.py
@@ -4,17 +4,21 @@ from turtle import Turtle, Screen
 timmy = Turtle()
 timmy.shape('turtle')
 run_timmy = True
-while run_timmy:
-    timmy.right(45)
 
 screen = Screen()
 
 print(screen.canvheight)
 
 
-def set_run_timmy():
-    False
+def set_run_timmy(coord1, coord2): # must pass the two arguments even if you don't use them
+    global run_timmy # must put this in order to change the outside variable
+    run_timmy = False
 
 
-screen.onscreenclick()
-screen.exitonclick()
+screen.onclick(set_run_timmy)
+# screen.exitonclick()
+
+while run_timmy:
+    timmy.right(45)
+
+

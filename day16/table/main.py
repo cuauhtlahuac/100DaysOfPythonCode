bb96c017f1433c5264fc6ae5d50f546f638c8efb
Rodrigo macos sierra <cuitlagua@gmail.com>
2022-03-24 21:46:38 -0600

    100py: print pokemon table challenge solved

--- day16/table/main.py
+++ day16/table/main.py
@@ -4,15 +4,14 @@ table_x = PrettyTable()
 table_x.set_style(MARKDOWN)
 table_x.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
 table_x.add_column("Type", ["Electric", "water", "Fire"])
-table_x.left_padding_width = 5
-table_x.right_padding_width = 5
-table_x.vertical_char = '*'
+table_x.align = 'r'
+
+print(table_x)
 
 """
-*     Pokemon Name     *       Type       *
-|:--------------------:|:----------------:|
-*       Pikachu        *     Electric     *
-*       Squirtle       *      water       *
-*      Charmander      *       Fire       *
+| Pokemon Name |     Type |
+|-------------:|---------:|
+|      Pikachu | Electric |
+|     Squirtle |    water |
+|   Charmander |     Fire |
 """
-print(table_x)

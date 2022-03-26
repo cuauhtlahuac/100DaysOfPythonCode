73cf5f22af76c9cea37f880caa4a2a771c44a72d
Rodrigo macos sierra <cuitlagua@gmail.com>
2022-03-26 08:25:57 -0600

    100py: Coffee machine OOP: print report

--- day16/oop_coffee_machine/oop-coffee-machine-start/main.py
+++ day16/oop_coffee_machine/oop-coffee-machine-start/main.py
@@ -1,3 +1,16 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+
+is_on = True
+
+while is_on:
+    choice = input("​What would you like? (espresso/latte/cappuccino): ")
+    if choice == "off":
+        is_on = False
+    if choice == 'report':
+        coffee_maker.report()
+        money_machine.report()

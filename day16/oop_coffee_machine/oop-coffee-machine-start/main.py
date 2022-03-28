52ada0091c4b80981ad334169d6d1e0088f89b13
Rodrigo macos sierra <cuitlagua@gmail.com>
2022-03-27 20:23:10 -0600

    100py: getting options from Menu Object

--- day16/oop_coffee_machine/oop-coffee-machine-start/main.py
+++ day16/oop_coffee_machine/oop-coffee-machine-start/main.py
@@ -1,14 +1,16 @@
-from menu import Menu, MenuItem
+from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
 money_machine = MoneyMachine()
 coffee_maker = CoffeeMaker()
+menu = Menu()
 
 is_on = True
 
 while is_on:
-    choice = input("​What would you like? (espresso/latte/cappuccino): ")
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options}): ")
     if choice == "off":
         is_on = False
     if choice == 'report':

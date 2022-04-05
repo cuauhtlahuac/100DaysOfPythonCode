d2efe29514d7f5d3428f124d822fc7ba43ac2a3a
Rodrigo macos sierra <cuitlagua@gmail.com>
2022-04-04 19:52:43 -0500

    100py: adding attributes with dot notation

--- day17/main.py
+++ day17/main.py
@@ -3,3 +3,9 @@ class User:
 
 
 user_1 = User()  # syntax to create an object from a class
+
+# You can add attributes with dot notation
+user_1.id = "001"
+user_1.name = "Juan Perez"
+
+print(user_1.name)

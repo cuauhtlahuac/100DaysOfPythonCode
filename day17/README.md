e98c3ac16a0361baaa90fc5cded290c8db685579
Rodrigo macos sierra <cuitlagua@gmail.com>
2022-04-04 19:47:29 -0500

    100py: creating an empty class an object

--- day17/README.md
+++ day17/README.md
@@ -1,1 +1,3 @@
 # Day 17 - The Quiz Project & the Benefits of OOP
+
+In this day we will build a Quiz Game
---
+++ day17/main.py
@@ -0,0 +1,5 @@
+class User:
+    pass  # to avoid the indentation error for emptiness
+
+
+user_1 = User()  # syntax to create an object from a class

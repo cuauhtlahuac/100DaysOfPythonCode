083669ad60344e1caf9692f0a1e01653572b71be
Rodrigo macos sierra <cuitlagua@gmail.com>
2022-04-04 20:32:03 -0500

    100py: adding attributes with default values

--- day17/main.py
+++ day17/main.py
@@ -2,8 +2,9 @@ class User:
     def __init__(self, user_id, user_name):
         self.id = user_id
         self.name = user_name
+        self.followers = 0  # default value
 
 
 user_1 = User("001", "Juan Perez")  # initialize with constructor def, in order to avoid typo errors
 
-print(user_1.name)
+print(user_1.followers)

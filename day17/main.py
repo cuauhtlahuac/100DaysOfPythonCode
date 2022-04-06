61841abd87a778f032fc930b5746dc5f596916e2
Rodrigo macos sierra <cuitlagua@gmail.com>
2022-04-05 21:00:53 -0500

    100py: add a method to the class

--- day17/main.py
+++ day17/main.py
@@ -4,7 +4,11 @@ class User:
         self.name = user_name
         self.followers = 0  # default value
 
+    # making a method
+    def follow(self):  # methods always need self as first parameter
+        pass
+
 
 user_1 = User("001", "Juan Perez")  # initialize with constructor def, in order to avoid typo errors
 
-print(user_1.followers)
+user_1.follow()

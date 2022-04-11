604f3d366b5f99980ea020ec94e3bd0598d18c83
rodrigo <cuitlagua@gmail.com>
2022-04-10 19:59:27 -0500

    100py: Quiz Project: Modeling Quiz brain

---
+++ day17/QuizProject/quiz_brain.py
@@ -0,0 +1,9 @@
+# TODO: asking the questions
+# TODO: checking if the answer was correct
+# TODO: checking if we're the end of the quiz
+
+class QuizBrain:
+    def __init__(self, question_list):
+        question_number = 0
+        question_list = question_list
+

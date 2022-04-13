82a7de97eec886ce6a761818e8eb95a167fdc184
rodrigo <cuitlagua@gmail.com>
2022-04-12 22:02:30 -0500

    100py: Quiz Project: style: quiz brain refactor, move to functions

--- day17/QuizProject/quiz_brain.py
+++ day17/QuizProject/quiz_brain.py
@@ -1,10 +1,28 @@
 from random import randint
 
 
+def get_user_answer(question):
+    return input(f'{question} - please type: True / False. ').capitalize()
+
+
+def handle_answer(user_answer=None, correct_answer=None):
+    if user_answer == correct_answer:
+        print("CORRECT")
+        return
+    print("WRONG!!!")
+    return
+
+
 class QuizBrain:
     def __init__(self, question_list):
         self.question_list = question_list
         self.quiz_continue = True
+
+    def __get_question(self, index):  # double underscore to make a private method
+        return self.question_list[index]
+
+    def __delete_question_from_list(self, index):
+        self.question_list.pop(index)
 
     # asking the questions
     def ask_question(self):
@@ -15,12 +33,9 @@ class QuizBrain:
             self.quiz_continue = False
         else:
             index = randint(0, length - 1)
-            question = self.question_list[index]
-            self.question_list.pop(index)
-            user_answer = input(f'{question.text} - please type: True / False. ').capitalize()
+            question = self.__get_question(index)
+            self.__delete_question_from_list(index)
+            user_answer = get_user_answer(question.text)
             # checking if the answer was correct
-            if user_answer == question.answer:
-                print("CORRECT")
-                return
-            print("WRONG!!!")
-            return
+            handle_answer(user_answer, question.answer)
+

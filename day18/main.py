7092da8c0551a171a83c2b1012c22f1b707827b4
Rodrigo macos sierra <cuitlagua@gmail.com>
2022-04-18 23:14:03 -0500

    100py: trying an example in docs

--- day18/main.py
+++ day18/main.py
@@ -1,6 +1,20 @@
-from turtle import Turtle, Screen
+from turtle import Turtle, Screen, color, begin_fill,forward,left,pos,end_fill,done
 
 timmy_the_turtle = Turtle()
+
+
+color('red', 'yellow')
+begin_fill()
+while True:
+    forward(200)
+    left(170)
+    if abs(pos()) < 1:
+        break
+end_fill()
+done()
+
+
+
 
 screen = Screen()
 

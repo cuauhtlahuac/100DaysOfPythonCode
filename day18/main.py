a6de217866f3bb7607793579df8d44762d505bcb
rodrigo <cuitlagua@gmail.com>
2022-04-19 22:30:25 -0500

    100py: making a little circle

--- day18/main.py
+++ day18/main.py
@@ -1,20 +1,18 @@
-from turtle import Turtle, Screen, color, begin_fill,forward,left,pos,end_fill,done
+from turtle import Turtle,\
+    Screen, color, begin_fill,\
+    forward, left, pos, end_fill, done
 
 timmy_the_turtle = Turtle()
-
 
 color('red', 'yellow')
 begin_fill()
 while True:
-    forward(200)
-    left(170)
+    forward(20)
+    left(20)
     if abs(pos()) < 1:
         break
 end_fill()
 done()
-
-
-
 
 screen = Screen()
 

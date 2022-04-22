16d2145a6198c3311601ad3bab857eb9a67a6d85
rodrigo <cuitlagua@gmail.com>
2022-04-21 21:17:43 -0500

    100py: drawing a square with for loop

--- day18/main.py
+++ day18/main.py
@@ -6,13 +6,9 @@ timmy_the_turtle.color('red', 'turquoise')
 
 timmy_the_turtle.begin_fill()
 
-timmy_the_turtle.forward(100)
-timmy_the_turtle.right(90)
-timmy_the_turtle.forward(100)
-timmy_the_turtle.right(90)
-timmy_the_turtle.forward(100)
-timmy_the_turtle.right(90)
-timmy_the_turtle.forward(100)
+for _ in range(4):
+    timmy_the_turtle.forward(100)
+    timmy_the_turtle.right(90)
 
 
 while True:

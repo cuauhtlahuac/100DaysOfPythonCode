296ff80c3186a3d1d014c3fc3a0835b7fa2f09c3
Rodrigo macos sierra <cuitlagua@gmail.com>
2022-04-25 23:49:42 -0500

    100py: draw a dash line

---
+++ day18/draw_dashed_line.py
@@ -0,0 +1,27 @@
+import turtle as t
+from turtle import Screen
+
+tim = t.Turtle()
+screen = Screen()
+
+########### Challenge 2 - Draw a Dashed Line ########
+win_width = screen.window_width()
+tim.shape("turtle")
+tim.pencolor('red')
+tim.penup()
+tim.setx(-1 * (win_width / 2))
+tim.width(3)
+tim.pendown()
+
+for _ in range(int(win_width / 20)):
+    print(tim.pos(), win_width)
+    if abs(tim.pos()) > win_width:
+        tim.right(90)
+
+    tim.forward(10)
+    tim.penup()
+    tim.forward(10)
+    tim.pendown()
+
+
+screen.exitonclick()

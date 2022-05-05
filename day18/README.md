cefa3c8f1862d6b50e93eefe42720bf3233fe41e
rodrigo <cuitlagua@gmail.com>
2022-05-04 22:05:31 -0500

    100py: random walk - use a rgb color approach

--- day18/README.md
+++ day18/README.md
@@ -1,5 +1,9 @@
 # Day 18:  165. Understanding Turtle Graphics and How to use the Documentation
 
+## Resources
+
+[RGB colors](https://www.w3schools.com/colors/colors_rgb.asp)
+
 ## python Turtle docs
 
 [link to docs](https://docs.python.org/3/library/turtle.html)
@@ -46,3 +50,7 @@ pip install heroes
 ```
 
 And then you'll import it as you did with other modules.
+
+## TUPLE
+
+Is a data type like a list but the values have immutability. You write it in stone
--- day18/random_walk.py
+++ day18/random_walk.py
@@ -1,10 +1,12 @@
-from turtle import Turtle, Screen
+from turtle import Turtle, Screen, colormode
 from css_colors import CSS_COLOR_NAMES
 from random import choice, randint
 
 screen = Screen()
-screen.bgcolor('#334')
 screen.setup(1000, 1000)
+colormode(255)  # changing the color mode to read floats (rgb)
+screen.bgcolor((0, 0, 0))
+
 middle_width = screen.window_width() / 2
 middle_height = screen.window_height() / 2
 right_limit = middle_width
@@ -12,19 +14,27 @@ left_limit = middle_width * -1
 top_limit = middle_height
 bottom_limit = middle_height * -1
 
-angles = [0, 90, 180, 270]
+angles = (0, 90, 180, 270)  # changing to tuple
 
 height = screen.window_height()
 width = screen.window_width()
 
 
 def choice_color():
-    return choice(CSS_COLOR_NAMES)
+    r = randint(0, 150)
+    g = randint(30, 255)
+    b = randint(0, 150)
+    return r, g, b
 
 
 def draw(distance=10):
     angle = choice(angles)
-    tim.color(choice_color())
+    choicer_color = choice_color()
+    tim.color(choicer_color)
+    if choicer_color[1] < 125:
+        tim.width(3)
+    else:
+        tim.width(1)
     x_pos = tim.pos()[0]
     y_pos = tim.pos()[1]
 

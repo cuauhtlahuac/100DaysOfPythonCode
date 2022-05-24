6342dbb1bea4aed9f1f2d05e54036001a6368252
Rodrigo macos sierra <cuitlagua@gmail.com>
2022-05-23 23:49:16 -0500

    100py: turtle race done!

--- day19/turtle_race/main.py
+++ day19/turtle_race/main.py
@@ -10,34 +10,51 @@ def turtles_run(runners, current_referee):
     """
     for runner in runners:
         runner.run()
-        have_winner = current_referee.set_winner(runner.x_position())
-        if have_winner:
-            print('Winner')
+        current_referee.set_game_over(runner)
+
+
+def get_color_list_text(colors_list):
+    colors_text = ''
+    for index in range(0, len(colors_list)):
+        color = colors_list[index]
+        colors_text += f"[{index + 1}]: {color}\n"
+    return colors_text
 
 
 screen = Screen()
+screen.title('Awesome Turtle Race')
+screen.bgcolor('lime green')
 screen_size = screen.screensize()
 screen_width = screen_size[0]
 screen_height = screen_size[1]
 
 referee = Referee(screen_width=screen_width, screen_height=screen_height)
+screen.tracer(2)
 referee.draw_field()
 
+colors = ('red', 'yellow', 'dark orange', 'dark green', 'deep pink', 'sienna')
 
-tim_red = TurtleRunner('black', (0, 0))
-tom_yellow = TurtleRunner('yellow', (-20, 0))
-tam_orange = TurtleRunner('orange', (-40, 0))
-tem_green = TurtleRunner('green', (-60, 0))
-tum_blue = TurtleRunner('lime', (-80, 0))
-bro_brown = TurtleRunner('brown', (-100, 0))
+tim_red = TurtleRunner(colors[0], (0, 0))
+tom_yellow = TurtleRunner(colors[1], (-20, 0))
+tam_orange = TurtleRunner(colors[2], (-40, 0))
+tem_green = TurtleRunner(colors[3], (-60, 0))
+tum_blue = TurtleRunner(colors[4], (-80, 0))
+bro_brown = TurtleRunner(colors[5], (-100, 0))
 
+screen.tracer(1)
 runners_list = (tim_red, tom_yellow, tam_orange, tem_green, tum_blue, bro_brown)
+referee.set_runners_list(runners_list)
 
-winner = False
+guessing = screen.textinput('Select the number of the winner', get_color_list_text(colors))
+referee.set_guessing(guessing)
 
-while not winner:
-    turtles_run(runners_list, referee)
+while not referee.terminated:
+    if referee.terminated:
+        break
+    else:
+        turtles_run(runners_list, referee)
 
+referee.final_positions()
 
 screen.exitonclick()
 
--- day19/turtle_race/turtleRunner.py
+++ day19/turtle_race/turtleRunner.py
@@ -5,14 +5,18 @@ from random import randint
 class TurtleRunner(Turtle):
     def __init__(self, color='lime', position=(0, 0)):
         super().__init__()
+        self.name = color
         self.color(color)
+        self.penup()
         self.sety(position[0])
         self.setx(-400)
         self.shape('turtle')
-        self.penup()
 
     def run(self):
-        self.forward(randint(10, 70))
+        self.forward(randint(10, 60))
 
     def x_position(self):
         return self.xcor()
+
+    def get_name(self):
+        return self.name

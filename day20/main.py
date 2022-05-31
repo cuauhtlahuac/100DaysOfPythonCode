1da9a0a0907aacdf8563edaa2341cadd2a1680eb
Rodrigo macos sierra <cuitlagua@gmail.com>
2022-05-30 22:44:41 -0500

    100py: move the snake

--- day20/main.py
+++ day20/main.py
@@ -4,10 +4,15 @@ screen = Screen()
 screen.bgcolor('black')
 snake = Turtle()
 snake.color('white')
+snake.penup()
+snake.speed(6)  # o to 10
 
 snake.shape("square")
-
 snake.shapesize(0.5, 1)
+
+
+while True:
+    snake.forward(1)
 
 
 screen.exitonclick()

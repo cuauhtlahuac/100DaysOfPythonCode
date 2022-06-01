648ae5e49e1b089461312b5be2aeb77c38a4e0f1
Rodrigo macos sierra <cuitlagua@gmail.com>
2022-05-31 22:09:52 -0500

    100py: Control the snake: using the arrows

--- day20/main.py
+++ day20/main.py
@@ -11,6 +11,29 @@ snake.shape("square")
 snake.shapesize(0.5, 1)
 
 
+def move_forwards():
+    snake.setheading(90)
+
+
+def move_backwards():
+    snake.setheading(270)
+
+
+def turn_left():
+    snake.setheading(180)
+
+
+def turn_right():
+    snake.setheading(0)
+
+
+screen.listen()
+screen.onkey(move_forwards, "Up")
+screen.onkey(move_backwards, "Down")
+screen.onkey(turn_left, "Left")
+screen.onkey(turn_right, "Right")
+
+
 while True:
     snake.forward(1)
 

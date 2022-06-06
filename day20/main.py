d4535c6369b2da2d53b78264246067164d1ce939
cuitlahuac from windows <cuitlagua@gmail.com>
2022-06-05 19:16:10 -0500

    100py: detect wall collision

--- day20/main.py
+++ day20/main.py
@@ -1,9 +1,26 @@
 from turtle import Turtle, Screen
 from food import Food
 
+
+def positive_limit(distance):
+    return (distance / 2) - 25
+
+
+def negative_limit(distance):
+    return ((distance / 2) - 25) * -1
+
+
 food = Food()
 
 screen = Screen()
+width = screen.window_width()
+height = screen.window_height()
+
+screen_R_limit = positive_limit(width)
+screen_L_limit = negative_limit(width)
+screen_T_limit = positive_limit(height)
+screen_B_limit = negative_limit(height)
+
 screen.bgcolor('black')
 snake = Turtle()
 snake.color('white')
@@ -36,10 +53,17 @@ screen.onkey(move_backwards, "Down")
 screen.onkey(turn_left, "Left")
 screen.onkey(turn_right, "Right")
 
+game_over = False
 
-while True:
+while not game_over:
     snake.forward(4)
     food.check_is_ate(snake)
+
+    if snake.xcor() >= screen_R_limit\
+       or snake.xcor() <= screen_L_limit\
+       or snake.ycor() >= screen_T_limit\
+       or snake.ycor() <= screen_B_limit:
+        game_over = True
 
 
 screen.exitonclick()

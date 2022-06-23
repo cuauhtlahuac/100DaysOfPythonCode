d2c26646cbc9944f1a4a33a4dd5a0c603ba5a948
Rodrigo macos sierra <cuitlagua@gmail.com>
2022-06-22 22:07:14 -0500

    100py: try to create a body and set the same position

--- day20/snake.py
+++ day20/snake.py
@@ -1,5 +1,12 @@
 from turtle import Turtle
 
+class Body(Turtle):
+    def __init__(self, x, y):
+        super().__init__()
+        self.snake_large = 1
+        self.color('white')
+        self.shape("square")
+        self.setpos(x, y)
 
 class Snake(Turtle):
     def __init__(self):
@@ -10,7 +17,7 @@ class Snake(Turtle):
         self.speed(6)  # o to 10
         self.shape("square")
         self.shapesize(1, self.snake_large)
+        self.body = []
 
     def snake_grow(self):
-        self.snake_large += 1
-        self.shapesize(1, self.snake_large)
+        self.body.append(Body(self.xcor(), self.ycor()))

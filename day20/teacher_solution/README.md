a665762d849c3658ffeaad3a412e809a13187335
Rodrigo Aristeo <cuitlagua@gmail.com>
2025-05-07 22:46:13 -0600

    feat: add initial implementation of the Snake game with turtle graphics

---
+++ day20/teacher_solution/README.md
@@ -0,0 +1,5 @@
+# Turtle notes
+
+Creamos una nueva tortuga, de 20 x 20, si queremos alinearlas tenemos que crearles las coordenadas.
+
+la posición del primer cuadro es (0,0), la siguiente esta 20 px a la izquierda, y la siguiente esta 40 px a la izquierda
---
+++ day20/teacher_solution/main.py
@@ -0,0 +1,36 @@
+from turtle import Screen, Turtle
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+
+# tree squares
+
+snake = Turtle()
+snake.shape("square")
+snake.color("white")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
